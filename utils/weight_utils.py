a9e5f527e8dc53dcba9152b4c70b3cd04957a83c
Chi Zhang <czhangseu@gmail.com>
2018-08-31 20:54:07 -0700

    fix layer bias init

--- utils/weight_utils.py
+++ utils/weight_utils.py
@@ -38,7 +38,6 @@ def fanin_init(layer):
 def xavier_init(m):
     if isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
         nn.init.xavier_normal_(m.weight)
-        nn.init.constant_(m.bias, 0)
     elif isinstance(m, nn.BatchNorm2d):
         nn.init.constant_(m.weight, 1)
         nn.init.constant_(m.bias, 0)
