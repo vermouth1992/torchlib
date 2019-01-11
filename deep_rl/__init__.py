646ef21dff4cc95aa90ed08935123c5f6d76e524
Chi Zhang <czhangseu@gmail.com>
2019-01-10 22:19:38 -0800

    fix deep_rl test shape

--- deep_rl/__init__.py
+++ deep_rl/__init__.py
@@ -42,7 +42,7 @@ def test(env, agent: BaseAgent, num_episode=100, frame_history_len=1, render=Fal
         while not done:
             if render:
                 env.render()
-            action = agent.predict(np.expand_dims(np.concatenate(observation_lst, axis=-1), axis=0))[0]
+            action = agent.predict(np.concatenate(observation_lst, axis=-1))
             previous_observation, reward, done, _ = env.step(action)
             episode_reward += reward
             observation_lst.pop(0)
