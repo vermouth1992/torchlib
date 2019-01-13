create .torchlib.json. Policy gradient action value share one network. Fix PPO logstd paramter bug

common.py
@@ -2,9 +2,10 @@
 import numpy as np
 import torch
 
+from . import enable_cuda
+
 __all__ = ['enable_cuda', 'FloatTensor', 'LongTensor', 'map_location', 'eps']
 
-enable_cuda = True if torch.cuda.is_available() else False
 FloatTensor = torch.cuda.FloatTensor if enable_cuda else torch.FloatTensor
 LongTensor = torch.cuda.LongTensor if enable_cuda else torch.LongTensor
 map_location = None if enable_cuda else 'cpu'

deep_rl/policy_gradient/ppo.py
@@ -13,20 +13,16 @@ from .utils import compute_gae, compute_sum_of_rewards
 
 
 class Agent(vanilla_pg.Agent):
-    def __init__(self, policy_net: nn.Module, policy_optimizer, discrete, nn_baseline,
-                 nn_baseline_optimizer, lam=1., clip_param=0.2, entropy_coef=0.01):
-        super(Agent, self).__init__(policy_net, policy_optimizer, discrete, nn_baseline,
-                                    nn_baseline_optimizer, lam)
+    def __init__(self, policy_net: nn.Module, policy_optimizer, discrete, lam=1., clip_param=0.2, entropy_coef=0.01,
+                 value_coef=0.5):
+        super(Agent, self).__init__(policy_net, policy_optimizer, discrete, True, lam, value_coef)
         self.clip_param = clip_param
         self.entropy_coef = entropy_coef
 
     def construct_dataset(self, paths, gamma):
         rewards = compute_sum_of_rewards(paths, gamma)
         observation = np.concatenate([path["observation"] for path in paths])
-        if self.nn_baseline:
-            advantage = compute_gae(paths, gamma, self.nn_baseline, self.lam, np.mean(rewards), np.std(rewards))
-        else:
-            advantage = rewards
+        advantage = compute_gae(paths, gamma, self.policy_net, self.lam, np.mean(rewards), np.std(rewards))
 
         # reshape all episodes to a single large batch
         actions = []
@@ -86,12 +82,11 @@ class Agent(vanilla_pg.Agent):
                 surr2 = torch.clamp(ratio, 1.0 - self.clip_param, 1.0 + self.clip_param) * advantage
                 policy_loss = -torch.min(surr1, surr2).mean()
 
-                loss = policy_loss - entropy_loss * self.entropy_coef
+                value_loss = self.get_baseline_loss(observation, discount_rewards)
+
+                loss = policy_loss - entropy_loss * self.entropy_coef + self.value_coef * value_loss
                 loss.backward()
                 self.policy_optimizer.step()
-
-                # update baseline
-                self.update_baseline(observation, discount_rewards)
 
 
 train = vanilla_pg.train

deep_rl/policy_gradient/utils.py
@@ -28,11 +28,11 @@ def compute_sum_of_rewards(paths, gamma):
     return rewards
 
 
-def compute_gae(paths, gamma, nn_baseline, lam, mean, std):
+def compute_gae(paths, gamma, policy_net, lam, mean, std):
     gaes = []
     for path in paths:
         with torch.no_grad():
-            values = nn_baseline.forward(torch.from_numpy(path['observation']).type(FloatTensor)).cpu().numpy()
+            values = policy_net.forward(torch.from_numpy(path['observation']).type(FloatTensor))[1].cpu().numpy()
         values = values * std + mean
         temporal_difference = path['reward'] + np.append(values[1:] * gamma, 0) - values
         # calculate reward-to-go

deep_rl/policy_gradient/vanilla.py
@@ -23,25 +23,23 @@ from .utils import compute_gae, compute_sum_of_rewards, sample_trajectories, pat
 
 
 class Agent(BaseAgent):
-    def __init__(self, policy_net: nn.Module, policy_optimizer, discrete, nn_baseline=None,
-                 nn_baseline_optimizer=None, lam=None):
+    def __init__(self, policy_net: nn.Module, policy_optimizer, discrete, nn_baseline=True, lam=None,
+                 value_coef=0.5):
         super(Agent, self).__init__()
         self.policy_net = policy_net
         self.policy_optimizer = policy_optimizer
         self.nn_baseline = nn_baseline
-        self.baseline_loss = None if not self.nn_baseline else nn.MSELoss()
-        if self.nn_baseline:
-            assert nn_baseline_optimizer is not None, "nn_baseline_optimizer can' be None"
-        self.nn_baseline_optimizer = nn_baseline_optimizer
+        self.baseline_loss = None if not nn_baseline else nn.MSELoss()
         self.discrete = discrete
         self.lam = lam
+        self.value_coef = value_coef
 
     def get_action_distribution(self, state):
         if self.discrete:
-            prob = self.policy_net.forward(state)
+            prob = self.policy_net.forward(state)[0]
             return Categorical(probs=prob)
         else:
-            mean, logstd = self.policy_net.forward(state)
+            mean, logstd = self.policy_net.forward(state)[0]
             return MultivariateNormal(mean, torch.exp(logstd))
 
     def predict(self, state):
@@ -67,22 +65,19 @@ class Agent(BaseAgent):
             action = self.get_action_distribution(state).sample(torch.Size([])).cpu().numpy()
             return action[0]
 
-    def update_baseline(self, observation, rewards):
+    def get_baseline_loss(self, observation, rewards):
         # update baseline
-        if self.nn_baseline:
-            self.nn_baseline_optimizer.zero_grad()
-            raw_baseline = self.nn_baseline.forward(observation)
-            rewards = (rewards - torch.mean(rewards)) / (torch.std(rewards) + eps)
-            rewards = rewards.type(FloatTensor)
-            loss = self.baseline_loss(raw_baseline, rewards)
-            loss.backward()
-            self.nn_baseline_optimizer.step()
+        raw_baseline = self.policy_net.forward(observation)[1]
+        rewards = (rewards - torch.mean(rewards)) / (torch.std(rewards) + eps)
+        rewards = rewards.type(FloatTensor)
+        loss = self.baseline_loss(raw_baseline, rewards)
+        return loss
 
     def construct_dataset(self, paths, gamma):
         rewards = compute_sum_of_rewards(paths, gamma)
         observation = np.concatenate([path["observation"] for path in paths])
         if self.nn_baseline:
-            advantage = compute_gae(paths, gamma, self.nn_baseline, self.lam, np.mean(rewards), np.std(rewards))
+            advantage = compute_gae(paths, gamma, self.policy_net, self.lam, np.mean(rewards), np.std(rewards))
         else:
             advantage = rewards
 
@@ -106,16 +101,14 @@ class Agent(BaseAgent):
         """
         actions, advantage, observation, rewards = dataset
 
-        observation = torch.tensor(observation).type(FloatTensor)
-        actions = torch.tensor(actions)
+        observation = torch.Tensor(observation).type(FloatTensor)
+        actions = torch.Tensor(actions)
         if enable_cuda:
             actions = actions.cuda()
-        advantage = torch.tensor(advantage).type(FloatTensor)
-        rewards = torch.tensor(rewards).type(FloatTensor)
+        advantage = torch.Tensor(advantage).type(FloatTensor)
+        rewards = torch.Tensor(rewards).type(FloatTensor)
 
         for _ in range(epoch):
-            self.update_baseline(observation, rewards)
-
             # update policy network
             self.policy_optimizer.zero_grad()
             # compute log prob
@@ -125,8 +118,14 @@ class Agent(BaseAgent):
             assert log_prob.shape == advantage.shape, 'log_prob length {}, advantage length {}'.format(log_prob.shape,
                                                                                                        advantage.shape)
 
-            policy_loss = torch.mean(-log_prob * advantage)
-            policy_loss.backward()
+            action_loss = torch.mean(-log_prob * advantage)
+            loss = action_loss
+
+            if self.nn_baseline:
+                value_loss = self.get_baseline_loss(observation, rewards)
+                loss = loss + value_loss * self.value_coef
+
+            loss.backward()
             self.policy_optimizer.step()
 
     def save_checkpoint(self, checkpoint_path, save_baseline=False):

examples/deep_rl/policy_gradient/ppo.py
@@ -24,46 +24,42 @@ class PolicyDiscrete(nn.Module):
             nn.Linear(state_dim, nn_size),
             nn.ReLU(),
             nn.Linear(nn_size, nn_size),
-            nn.ReLU(),
+            nn.ReLU()
+        )
+        self.action_head = nn.Sequential(
             nn.Linear(nn_size, action_dim),
             nn.Softmax(dim=-1)
         )
+        self.value_head = nn.Linear(nn_size, 1)
 
     def forward(self, state):
-        return self.model.forward(state)
+        x = self.model.forward(state)
+        action = self.action_head.forward(x)
+        value = self.value_head.forward(x)
+        return action, value.squeeze(-1)
 
 
 class PolicyContinuous(nn.Module):
     def __init__(self, nn_size, state_dim, action_dim):
         super(PolicyContinuous, self).__init__()
-        self.logstd = torch.randn([action_dim, action_dim], requires_grad=True).type(FloatTensor)
+        self.logstd = torch.nn.Parameter(torch.randn([action_dim, action_dim], requires_grad=True).type(FloatTensor))
         self.model = nn.Sequential(
             nn.Linear(state_dim, nn_size),
             nn.ReLU(),
             nn.Linear(nn_size, nn_size),
             nn.ReLU(),
+        )
+        self.action_head = nn.Sequential(
             nn.Linear(nn_size, action_dim),
             nn.Tanh()
         )
+        self.value_head = nn.Linear(nn_size, 1)
 
     def forward(self, state):
-        mean = self.model.forward(state)
-        return mean, self.logstd
-
-
-class Baseline(nn.Module):
-    def __init__(self, nn_size, state_dim):
-        super(Baseline, self).__init__()
-        self.model = nn.Sequential(
-            nn.Linear(state_dim, nn_size),
-            nn.ReLU(),
-            nn.Linear(nn_size, nn_size),
-            nn.ReLU(),
-            nn.Linear(nn_size, 1),
-        )
-
-    def forward(self, state):
-        return self.model.forward(state).squeeze(dim=-1)
+        x = self.model.forward(state)
+        mean = self.action_head.forward(x)
+        value = self.value_head.forward(x)
+        return (mean, self.logstd), value.squeeze(-1)
 
 
 def make_parser():
@@ -75,11 +71,11 @@ def make_parser():
     parser.add_argument('--gae_lambda', type=float, default=0.98)
     parser.add_argument('--clip_param', type=float, default=0.2)
     parser.add_argument('--entropy_coef', type=float, default=0.01)
+    parser.add_argument('--value_coef', type=float, default=0.5)
     parser.add_argument('--n_iter', '-n', type=int, default=100)
     parser.add_argument('--batch_size', '-b', type=int, default=1000)
     parser.add_argument('--ep_len', '-ep', type=float, default=-1.)
     parser.add_argument('--learning_rate', '-lr', type=float, default=5e-3)
-    parser.add_argument('--nn_baseline', '-bl', action='store_true')
     parser.add_argument('--nn_size', '-s', type=int, default=64)
     parser.add_argument('--seed', type=int, default=1)
     return parser
@@ -119,19 +115,10 @@ if __name__ == '__main__':
 
     policy_optimizer = torch.optim.Adam(policy_net.parameters(), args.learning_rate)
 
-    if args.nn_baseline:
-        baseline_net = Baseline(args.nn_size, ob_dim)
-        baseline_optimizer = torch.optim.Adam(baseline_net.parameters(), args.learning_rate)
-        gae_lambda = args.gae_lambda
-        if enable_cuda:
-            baseline_net.cuda()
-    else:
-        baseline_net = None
-        baseline_optimizer = None
-        gae_lambda = None
+    gae_lambda = args.gae_lambda
 
-    agent = ppo.Agent(policy_net, policy_optimizer, discrete, baseline_net, baseline_optimizer, gae_lambda,
-                      clip_param=args.clip_param, entropy_coef=args.entropy_coef)
+    agent = ppo.Agent(policy_net, policy_optimizer, discrete, gae_lambda, clip_param=args.clip_param,
+                      entropy_coef=args.entropy_coef, value_coef=args.value_coef)
 
     ppo.train(args.exp_name, env, agent, args.n_iter, args.discount, args.batch_size, max_path_length,
               logdir=None, seed=args.seed)