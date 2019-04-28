204627e3102da32f455e8402adfd0eaa00773297
vermouth1992 <czhangseu@gmail.com>
2019-04-28 00:41:01 -0700

    add mask linear and masked autoencoder density estimation (MADE)

--- utils/layers.py
+++ utils/layers.py
@@ -2,12 +2,13 @@
 A set of pytorch layer utilities for fast network building
 """
 
+import numpy as np
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from torch.autograd import Variable
-
-from .weight import apply_weight_norm
-from ..common import enable_cuda
+from torchlib.common import enable_cuda
+from torchlib.utils.weight import apply_weight_norm
 
 """
 Basic layer utils
@@ -200,6 +201,20 @@ class DynamicGNoise(nn.Module):
         self.noise.data.normal_(0, std=self.std)
 
         return x + self.noise.expand(x.shape)
+
+
+class MaskedLinear(nn.Linear):
+    """ same as Linear except has a configurable mask on the weights """
+
+    def __init__(self, in_features, out_features, bias=True):
+        super().__init__(in_features, out_features, bias)
+        self.register_buffer('mask', torch.ones(out_features, in_features))
+
+    def set_mask(self, mask):
+        self.mask.data.copy_(torch.from_numpy(mask.astype(np.uint8).T))
+
+    def forward(self, input):
+        return F.linear(input, self.mask * self.weight, self.bias)
 
 
 """
