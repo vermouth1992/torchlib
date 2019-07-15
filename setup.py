f37968456971b5f576b65154743562635efc3588
vermouth1992 <czhangseu@gmail.com>
2019-07-14 21:38:03 -0700

    add setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='torchlib',
+    version='0.1',
+    packages=['torchlib'],
+    url='https://github.com/vermouth1992/torchlib',
+    license='MIT',
+    author='Chi Zhang',
+    author_email='czhangseu@gmail.com',
+    description=''
+)
