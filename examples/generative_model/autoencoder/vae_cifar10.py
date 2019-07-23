b204334b0c6a409276b5abf32b12ab5b6ec7fc76
vermouth1992 <czhangseu@gmail.com>
2019-07-23 14:26:42 -0700

    vae cifar10

--- examples/generative_model/autoencoder/vae_cifar10.py
+++ examples/generative_model/autoencoder/vae_cifar10.py
@@ -8,8 +8,8 @@ import torch.nn as nn
 from tensorboardX import SummaryWriter
 from torchvision import transforms
 
-from torchlib.dataset.image.cifar10 import get_cifar10_data_loader
-from torchlib.generative_model.autoencoder.utils import SampleImage
+from torchlib.dataset.image.cifar10 import get_cifar10_data_loader, get_cifar10_subset_data_loader
+from torchlib.generative_model.autoencoder.utils import SampleImage, Reconstruction, VisualizeLatent
 from torchlib.generative_model.autoencoder.vae import VAE, Trainer
 from torchlib.utils.layers import conv2d_bn_lrelu_block, conv2d_trans_bn_lrelu_block
 
@@ -19,7 +19,7 @@ class Encoder(nn.Module):
         super(Encoder, self).__init__()
 
         self.model = nn.Sequential(
-            *conv2d_bn_lrelu_block(3, 32, 4, 2, 1),
+            *conv2d_bn_lrelu_block(in_channels=3, out_channels=32, kernel_size=4, stride=2, padding=1),
             *conv2d_bn_lrelu_block(32, 32, 4, 2, 1),
             *conv2d_bn_lrelu_block(32, 32, 4, 2, 1),
         )
@@ -28,8 +28,8 @@ class Encoder(nn.Module):
         self.logvar = nn.Linear(512, code_size)
 
     def forward(self, img):
-        img_flat = img.view(img.shape[0], -1)
-        x = self.model(img_flat)
+        x = self.model(img)
+        x = x.view(x.shape[0], -1)
         mu = self.mu(x)
         logvar = self.logvar(x)
         return mu, logvar
@@ -41,9 +41,9 @@ class Decoder(nn.Module):
         self.linear = nn.Linear(code_size, 512)
 
         self.model = nn.Sequential(
-            *conv2d_trans_bn_lrelu_block(32, 32, kernel_size=4, stride=2, padding=1),
-            *conv2d_trans_bn_lrelu_block(32, 32, kernel_size=4, stride=2, padding=1),
-            *conv2d_trans_bn_lrelu_block(32, 3, kernel_size=4, stride=2, padding=1),
+            *conv2d_trans_bn_lrelu_block(in_channels=32, out_channels=32, kernel_size=4, stride=2, padding=1),
+            *conv2d_trans_bn_lrelu_block(in_channels=32, out_channels=32, kernel_size=4, stride=2, padding=1),
+            *conv2d_trans_bn_lrelu_block(in_channels=32, out_channels=3, kernel_size=4, stride=2, padding=1),
             nn.Sigmoid()
         )
 
@@ -57,6 +57,7 @@ class Decoder(nn.Module):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='VAE for cifar10')
     parser.add_argument('--train', action='store_true')
+    parser.add_argument('--batch_size', type=int, default=128)
     parser.add_argument('--resume', choices=['model', 'checkpoint'])
     parser.add_argument('--epoch', required='--train' in sys.argv)
     args = vars(parser.parse_args())
@@ -80,14 +81,14 @@ if __name__ == '__main__':
     model = VAE(generator, decoder, code_size, optimizer)
 
     sampler = SampleImage(10, 10)
-    # data_loader = get_mnist_subset_data_loader(train=True, transform=transform, fraction=100)
-    # reconstruct = Reconstruction(next(iter(data_loader))[0])
+    data_loader = get_cifar10_subset_data_loader(train=True, transform=transform, fraction=100)
+    reconstruct = Reconstruction(next(iter(data_loader))[0])
 
-    summary_writer = SummaryWriter('runs/vae_mnist')
+    summary_writer = SummaryWriter('runs/vae_cifar10')
 
     if train:
-        # visualize_data_loader = get_mnist_subset_data_loader(train=True, transform=transform, fraction=1000)
-        # visualize_callback = VisualizeLatent(visualize_data_loader, method='pca')
+        visualize_data_loader = get_cifar10_subset_data_loader(train=True, transform=transform, fraction=1000)
+        visualize_callback = VisualizeLatent(visualize_data_loader, method='pca')
         resume = args['resume']
         num_epoch = int(args['epoch'])
 
@@ -96,11 +97,11 @@ if __name__ == '__main__':
         elif resume == 'checkpoint':
             model.load_checkpoint(checkpoint_path, all=True)
 
-        train_data_loader = get_cifar10_data_loader(train=True, transform=transform)
+        train_data_loader = get_cifar10_data_loader(train=True, transform=transform, batch_size=args['batch_size'])
 
         trainer = Trainer(recon_loss_f)
-        # trainer.train(num_epoch, train_data_loader, model, checkpoint_path, epoch_per_save=10,
-        #               callbacks=[sampler, reconstruct, visualize_callback], summary_writer=summary_writer)
+        trainer.train(num_epoch, train_data_loader, model, checkpoint_path, epoch_per_save=10,
+                      callbacks=[sampler, reconstruct, visualize_callback], summary_writer=summary_writer)
 
         trainer.train(num_epoch, train_data_loader, model, checkpoint_path, epoch_per_save=10,
                       callbacks=[sampler], summary_writer=summary_writer)
--- torchlib/dataset/image/cifar10.py
+++ torchlib/dataset/image/cifar10.py
@@ -3,9 +3,11 @@ Cifar10 dataloader
 """
 
 import torch
+from torch.utils.data.sampler import SubsetRandomSampler
+from torchvision import datasets, transforms
+
 from torchlib.common import enable_cuda
 from torchlib.dataset import dataset_root_path
-from torchvision import datasets, transforms
 
 
 def get_cifar10_default_transform(train, augmentation=False):
@@ -34,6 +36,20 @@ def get_cifar10_data_loader(train, batch_size=128, transform=None, augmentation=
     kwargs = {'num_workers': 0, 'pin_memory': True} if enable_cuda else {}
     dataset = get_cifar10_dataset(train, transform, augmentation)
     train_loader = torch.utils.data.DataLoader(dataset, batch_size, shuffle=True, **kwargs)
+    return train_loader
+
+
+def get_cifar10_subset_data_loader(train, batch_size=128, transform=None, fraction=0.1):
+    kwargs = {'num_workers': 1, 'pin_memory': True} if enable_cuda else {}
+    dataset = get_cifar10_dataset(train, transform, augmentation=False)
+    if type(fraction) == float:
+        end = int(len(dataset) * fraction)
+    elif type(fraction) == int:
+        end = fraction
+    else:
+        raise ValueError('fraction has to be float or int')
+    sampler = SubsetRandomSampler(list(range(0, end)))
+    train_loader = torch.utils.data.DataLoader(dataset, batch_size, sampler=sampler, shuffle=False, **kwargs)
     return train_loader
 
 
